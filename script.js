d4d2a4fb0c9d9715a974cf8797314fd83af4cc29
arora <aroramadhav65@gmail.com>
2022-08-23 15:21:21 +0530

    Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var menuIcon = document.querySelector(".menu-icon");
-var menuIcon = document.querySelector(".menu-icon");
+var menuIcon = document.querySelector(".sidebar");
 
 menuIcon.onclick = function () {
     sidebar.classList.toggle("small-sidebar");
